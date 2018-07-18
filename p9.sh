c1ca02d1af85f47b46abe424fc04e0ec568a52a1
Varun <varunkmr26@gmail.com>
2018-07-18 12:26:44 +0530

    Update tomcat solution

--- p9.sh
+++ p9.sh
@@ -5,18 +5,14 @@ if [ "$1" != "" ]; then
 else
     port="8080"
 fi
+SCRIPT="echo \"Connected to the remote server.....\";echo \"Installing system updates.....\";sudo yum update -y;echo \"Installing tomcat....\";sudo yum install tomcat -y;
+echo \"Making a new directory for the application....\";sudo mkdir /usr/share/tomcat/webapps/new-app;echo \"Making the index file....\";sudo touch /usr/share/tomcat/webapps/new-app/index.html;echo 'This is a test application' | sudo tee --append /usr/share/tomcat/webapps/new-app/index.html;echo \"Changing the default port no....\";sudo sed -i \"s/port=\"8080\"/port=\"$port\"/g\"  /usr/share/tomcat/conf/server.xml;echo \"Starting tomcat server....\";sudo service tomcat start"
+echo "Please enter the address of the host:"
+read HOST
+echo "Please enter the name of the private key file(with path and extension):"
+read PEM
+echo "Connecting to client"
+ssh -i $PEM ec2-user@$HOST "${SCRIPT}"
 
-echo "Installing updates"
-sudo yum update -y
-echo "Installing tomcat"
-sudo yum install tomcat -y
-echo "make a new directory"
-sudo mkdir /usr/share/tomcat/webapps/new-app
-echo "make the index file"
-echo "This is my-app" > /usr/share/tomcat/webapps/new-app/index.html
-echo "Change the port no"
-sudo sed -i "s/Connector port=\"8080\"/Connector port=\"$port\"/g"  /usr/share/$
-echo "Starting tomcat server"
-sudo service tomcat start
 
 
